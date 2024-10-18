6c7ec3ad4b023ad86e7727cfef0c8ef2b6f935ce
its-Jaustin <jacksoa2@mail.gvsu.edu>
2024-10-18 10:53:44 -0400

    added 1.two_sum

---
+++ hash-table/01.two_sum.py
@@ -0,0 +1,44 @@
+#01.two_sum.py
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+
+#Time Complexity: O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #use a dict to store indecies
+        hashed = {}
+        m = len(nums)
+        
+        for i in range(m):
+            #check if solution is hashed
+            if (target - nums[i]) in hashed:
+                return [hashed[target-nums[i]], i]
+            #place in hash
+            hashed[nums[i]] = i
+        return [] #if no solution found
+
+    
+
+
+        
+        
+
