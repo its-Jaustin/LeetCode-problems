6391f4d4c2e2c114cea54c9115cd0add47d8cbe0
its-Jaustin <123894988+its-Jaustin@users.noreply.github.com>
2024-11-12 19:07:57 -0500

    Update 200.NumberOflslands.py

--- 200.NumberOflslands.py
+++ 200.NumberOflslands.py
@@ -1,16 +1,29 @@
+#200. Number of Islands
+#Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+#An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+#Constraints:
+#m == grid.length
+#n == grid[i].length
+#1 <= m, n <= 300
+#grid[i][j] is '0' or '1'.
+
+#Time Complexity: O(x*y)
+
 class Solution:
     def numIslands(self, grid: list[list[str]]) -> int:
         self.grid = grid
-        self.checked = set()
-        checked = self.checked
+        checked = set()
         islands = 0
+        m = len(grid) - 1
+        n = len(grid[0]) -1
         for x in range(len(grid)):
             for y in range(len(grid[0])):
                 if (x,y) in checked: continue #move on if already checked
-                if grid[x][y] == '1':
+                elif grid[x][y] == '1':
                     checked.add((x,y))
                     islands += 1
-                    self.visualize(checked)
                 else: #if '0' move on to next
                     checked.add((x,y))
                     continue
@@ -19,47 +32,16 @@ class Solution:
                 while level != []:
                     key = level.pop()
                     checked.add(key)
-                    kids = [x for x in self.getAdjacent(key) if x not in checked and grid[x[0]][x[1]]== '1']
+                    #this would be faster if we didn't use a custom function but I didn't want to
+                    kids = [x for x in self.getAdjacent(key, m, n) if x not in checked and grid[x[0]][x[1]]== '1']
                     for k in kids:
                         level.append(k)
                         checked.add(k)
-                    print((x,y))
-                    self.visualize(checked)
         return islands
-    def getAdjacent(self, p:tuple) -> list:
+    def getAdjacent(self, p:tuple, m, n) -> list:
         adj = []
         if p[0] > 0: adj.append((p[0]-1, p[1]))
-        if p[0] < (len(self.grid) - 1): adj.append((p[0]+1, p[1]))
+        if p[0] < (m): adj.append((p[0]+1, p[1]))
         if p[1] > 0: adj.append((p[0], p[1]-1))
-        if p[1] < (len(self.grid[0]) - 1): adj.append((p[0], p[1]+1))
+        if p[1] < (n): adj.append((p[0], p[1]+1))
         return adj
-    def visualize(self, checked):
-        v_grid = [row.copy() for row in self.grid]
-        for key in checked:
-            v_grid[key[0]][key[1]] = 'X'
-        for row in v_grid: print(row)
-
-
-if __name__ == '__main__':
-    grid = [
-        ["1","1","1","1","0"],
-        ["1","1","0","1","0"],
-        ["1","1","0","0","0"],
-        ["0","0","0","0","0"]]
-    grid2 =[
-        ["1","1","0","0","0"],
-        ["1","1","0","0","0"],
-        ["0","0","1","0","0"],
-        ["0","0","0","1","1"]]
-    grid3 = [
-        ["1","0","1","1","1"],
-        ["1","0","1","0","1"],
-        ["1","1","1","0","1"]]
-    grid4 = [
-        ["1","0","1","0","1"],
-        ["1","1","0","1","0"],
-        ["1","0","0","1","1"]]
-    for row in grid4: print(row)
-    print()
-    islands = Solution().numIslands(grid3)
-    print("Num islands:", islands)
